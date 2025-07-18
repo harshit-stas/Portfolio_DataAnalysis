8f36831579bc0a42324eb75034aa757d6466ca7c
Harshit Raman <121501728+harshit-stas@users.noreply.github.com>
2025-07-18 23:02:57 +0530

    Update ProjectCard.js

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,1 +1,9 @@
-
+export default function ProjectCard({ title, tools, description }) {
+  return (
+    <div className="bg-gray-800 shadow-md rounded-xl p-6 hover:shadow-lg transition border border-gray-700">
+      <h2 className="text-xl font-semibold text-white mb-2">{title}</h2>
+      <p className="text-sm text-blue-400 mb-2">Tools: {tools}</p>
+      <p className="text-gray-300">{description}</p>
+    </div>
+  );
+}
